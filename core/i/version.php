d193cecac9b673ee7394ec169b3d370a38b565fb
stephan.kasdorf <s.kasdorf@maschinen-stockert.de>
2023-08-17 16:16:49 +0200

    nibiru framework v0.9.7 - added a new README.md to have a better understanding for the framework itself, added a versioning system with version numbers.

---
+++ core/i/version.php
@@ -0,0 +1,15 @@
+<?php
+namespace Nibiru\Adapter;
+/**
+ * interface   IVersion
+ * @project    Nibiru Framework
+ * @desc       This is a PHP interface file, please specify the use
+ * @author     Stephan Kasdorf
+ * @date       17.08.23
+ * @time       11:25
+ * @package    Nibiru\Adapter
+ */
+interface IVersion
+{
+    const NIBIRU_VERSION = "0.9.7";
+}
